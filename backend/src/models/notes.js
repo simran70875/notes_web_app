8aeaf05d6f227ef8937b0b0f6c869cfd6691e885
Simran kaur <116719089+simran70875@users.noreply.github.com>
2024-08-21 16:21:43 +0530

    Delete backend directory

--- backend/src/models/notes.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const noteSchema = new mongoose.Schema({
-  userID: { type: mongoose.Schema.Types.ObjectId, required: true},
-  title: { type: String, required: true, },
-  content: { type: String, required: true },
-});
-
-module.exports = mongoose.model("notes", noteSchema);
